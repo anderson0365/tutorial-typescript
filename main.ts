e5cff9dc32f3ae3995e4d5b304b2e7199204d55e
Anderson Luis Cáceres Padilla <al.caceres@uniandes.edu.co>
2021-04-22 06:30:14 -0500

    Tarea 4

--- main.ts
+++ main.ts
@@ -8,4 +8,16 @@ export const ap = new Aprendiz(
   NivelEducativo.POSGRADO
 );
 
-console.log(ap);
+let aprendizTable: HTMLElement = document.getElementById("aprendiz")!;
+
+mostrarDatosAprendiz(ap);
+
+function mostrarDatosAprendiz(aprendiz: Aprendiz): void {
+  let tbodyAprendiz = document.createElement("tbody");
+  tbodyAprendiz.innerHTML = `<tr><td colspan=2><img src="./img/${aprendiz.avatar}" height="100"></td></tr>
+  <tr><td>Nombres: </td><td>${aprendiz.nombres}</td></tr>
+  <tr><td>Apellidos: </td><td>${aprendiz.aplellidos}</td></tr>
+  <tr><td>Nivel educativo: </td><td>${aprendiz.nivelEducativo}</td></tr>
+  <tr><td>Edad: </td><td>${aprendiz.edad}</td></tr>`;
+  aprendizTable.appendChild(tbodyAprendiz);
+}
